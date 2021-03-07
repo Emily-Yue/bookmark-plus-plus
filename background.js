fcb3f72fa6462a4f3ec72a8be7a550f771a965aa
PLM 1 <arushi.harkawat@gmail.com>
2021-03-07 11:30:21 -0600

    IT WORKSSS

--- background.js
+++ background.js
@@ -43,19 +43,130 @@ document.addEventListener('DOMContentLoaded', function() {
             document.body.appendChild(div)
         }
 
+        // filter!!
+        /**
+        * Create a function to generate your elements based
+        * off the passed in array of data
+        */
+        function makeList(data) {
+            var paras = document.getElementsByClassName("websiteBlock")
+            while (paras[0]) {
+                paras[0].parentNode.removeChild(paras[0])
+            }
+            // Iterate through your data and create the elements
+            // and append them to the container
+            // data.forEach(i => {
+            // // const element = document.createElement('div');
+            // // element.innerText = i.name;
+            // // container.append(element);
+            // document.body.appendChild(i)
+            // });
+            // console.log("Data: " + data)
+            for (const d of data) {
+                if (d instanceof HTMLDivElement) {
+                    document.body.appendChild(d)
+                } else {
+                    var div = document.createElement("div")
+                    div.setAttribute("class", "websiteBlock")
 
-        // Object.keys(items).forEach(function (bookmark){
-        //     //console.log(items.length)
+                    var a = document.createElement("a")
+                    a.setAttribute("href", d.url)
+                    a.setAttribute("class", "link")
+                    a.innerHTML = d.title
+                    div.appendChild(a)
+                    var br = document.createElement("br")
+                    div.appendChild(br)
+
+                    var br1 = document.createElement("br")
+                    div.appendChild(br1)
+                    console.log(d.tags)
+                    var tagsList = d.tags
+                    for(var tag of tagsList){
+                        var divTag = document.createElement("div")
+                        divTag.setAttribute("class", "tag")
+                        divTag.innerHTML = tag
+                        div.appendChild(divTag)
+                    }
+                    //div.textContent = value.url
+                    document.body.appendChild(div)
+                }
+            }
+        }
+        
+        /**
+         * Create an event listener to react to
+         * search updates so you can filter the list.
+         * keyUp is used so that you wait for the
+         * user to actually finish typing that specific
+         * char before running. Otherwise, you'll be missing
+         * a char. (Try changing it to 'keypress' and see what happens)
+         */
+        document.getElementById('query').addEventListener('keyup', function(e) {
+            // Get the textbox value
+            const searchTerm = e.target.value;
+            // If no value, reset the list to all items
+            if (!searchTerm) {
+                makeList(items.data);
+                return;
+            }
+            // Filter your list of data
+            // based off the searchTerm
+            //const data = items.filter(i => i.name.toLowerCase().includes(searchTerm.toLowerCase()));
+            const allWebsiteBlocks = document.getElementsByClassName("websiteBlock")
+            //console.log(allWebsiteBlocks)
+            const filteredWebsites = Array.prototype.filter.call(allWebsiteBlocks, function(webBlock) {
+                var currTags = webBlock.getElementsByClassName("tag")
+                var isFound = false
+                for(var tag of currTags){
+                    isFound = isFound || tag.innerHTML.includes(searchTerm)
+                }
+                return (
+                    webBlock.childNodes[0].innerHTML.includes(searchTerm) || isFound
+                )
+            })
+            // const allWebsiteBlocks = []
+            // for (const wb of allWebsiteBlocks)
             
-        //     var realBookmark = items.bookmark
-        //     const div = document.createElement('div')
-        //     //console.log(realBookmark)
-        //     //console.log(title)
-        //     //var timeFormat = realBookmark.timeCreated.getMonth() + "/" + realBookmark.timeCreated.getDate() + "/" + realBookmark.timeCreated.getFullYear();
-        //     div.textContent = `${realBookmark.url}: ${realBookmark.title}, ${realBookmark.tags}`
-        //     console.log(div.textContent)
-        //     document.body.appendChild(div)
-        // })
+            // Pass the list filtered list of data to your makeList function
+            // to generate your html
+            //console.log(filteredWebsites)
+            makeList(filteredWebsites);
+        });
+        
+        // Generate your initial list
+        makeList(items.data);
+
+
+        // const input = document.getElementById("query")
+        // input.addEventListener("keyup", e => {
+        //     const searchString = e.target.value
+        //     console.log(searchString)
+        //     const allWebsiteBlocks = document.getElementsByClassName("websiteBlock")
+            // const filteredWebsites = Array.prototype.filter.call(allWebsiteBlocks, function(webBlock) {
+            //     var currTags = webBlock.getElementsByClassName("tag")
+            //     var isFound = false
+            //     for(var tag of currTags){
+            //         isFound = isFound || tag.innerHTML.includes(searchString)
+            //     }
+            //     console.log(isFound)
+            //     return (
+            //         webBlock.childNodes[0].innerHTML.includes(searchString) || isFound
+            //     )
+            // })
+        //     console.log("filteredWesbites next")
+        //     console.log(filteredWebsites)
+        //     //document.querySelectorAll('websiteBlock').forEach(e => e.remove())
+        //     var paras = document.getElementsByClassName("websiteBlock")
+
+        //     while (paras[0]) {
+        //         paras[0].parentNode.removeChild(paras[0])
+        //     }
+            // for (const value of filteredWebsites) {
+            //     // console.log(value)
+            //     // var div = document.createElement("div")
+            //     // div.setAttribute("class", "websiteBlock")
+            //     document.body.appendChild(value)
+            // }
         
     })
     //chrome.storage.local.clear()
